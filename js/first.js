aff8882fecaf31908e370bcf59718ca5c9a1827d
linqiang69672187 <69672187@qq.com>
2018-12-25 16:09:22 +0800

    sd

--- js/first.js
+++ js/first.js
@@ -8,6 +8,53 @@
 var app = new Vue({
     el: '#app-2',
     data: {
-        message: '页面加载于 ' + new Date().toLocaleString()
+        message: '页面加载于 ' + new Date().toLocaleString(),
     }
-})
+});
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen:true
+    }
+});
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '整个牛项目' }
+      ]
+    }
+  });
+
+  var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hello Vue.js!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  });
+  
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'Hello Vue!'
+    }
+});
