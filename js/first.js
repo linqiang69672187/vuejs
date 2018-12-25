16ab56e2339b3206a569cedfd341b6d3f55371db
linqiang69672187 <69672187@qq.com>
2018-12-25 15:48:33 +0800

    sd

---
+++ js/first.js
@@ -0,0 +1,13 @@
+﻿var app111 = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hell2222o Vue!',
+        nihao:'sdfsdfsdf'
+    }
+});
+var app = new Vue({
+    el: '#app-2',
+    data: {
+        message: '页面加载于 ' + new Date().toLocaleString()
+    }
+})
