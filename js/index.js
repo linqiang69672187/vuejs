ea1153add5510ab93fbeb5acdf5744f8a9f62a95
linqiang_f <69672187@qq.com>
2019-02-27 09:54:07 +0800

    1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-var app = new Vue({
+﻿var app = new Vue({
     el:'#app',
     data:{
-        activeKey:'1'
+        activeKey: '1',
+        books: [{ 'name': 'VUEJS实战' }, { 'name': 'CSS解密' }, { 'name': 'JAVA参考手册' }]
     }
 });
