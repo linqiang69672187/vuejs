d3f4614feae3b21a26fb4bb2bdf4dfbc04c07b17
linqiang_f <69672187@qq.com>
2019-03-01 14:16:56 +0800

    11

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
 ﻿var app = new Vue({
     el:'#app',
     data:{
-        activeKey: '1',
-        books: [{ 'name': 'VUEJS实战' }, { 'name': 'CSS解密' }, { 'name': 'JAVA参考手册' }]
+        list: [
+            { 'id': 1, 'name': 'iPhone 7', 'price': 6188, 'count': 1 },
+            { 'id': 2, 'name': 'iPad Pro', 'price': 8555, 'count': 1 },
+            { 'id': 3, 'name': 'MacBook Pro', 'price': 28555, 'count': 1 }
+        ]
+    },
+    created:function(){
+        console.log(this.activeKey);
+    },
+    mounted: function () {
+
+    },
+    methods: {
+        handleRd: function (index) {
+            this.list[index].count--;
+        },
+        handleAdd: function (index) {
+            this.list[index].count++;
+        },
+        remove: function (index) {
+            this.list.splice(index, 1);
+        }
+    },
+    computed: {
+        total: function () {
+            var totalvalue = 0;
+            for (let li = 0; li < this.list.length;li++) {
+                totalvalue += this.list[li].price * this.list[li].count;
+            };
+            return totalvalue;
+        },
     }
 });
